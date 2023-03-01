03dfb2a2efdcaf521e72fb4910ee76fbb2e6091c
garcia jonathan <jonathanegarcia33@gmail.com>
2023-03-01 13:19:45 -0300

    proyecto final cv

---
+++ java/function.js
@@ -0,0 +1,14 @@
+document.getElementById("boton contraste").addEventListener('click', function(){
+    document.body.style.backgroundColor= 'gray'
+
+})
+document.getElementById("Fondo Blanco").addEventListener('click', function(){
+    document.body.style.backgroundColor= 'white'
+
+})
+document.getElementById("Ocultar referencia").addEventListener('click', function(){
+    document.getElementById('referencia').style.display= 'none';
+    document.getElementById('referencia1').style.display= 'none';
+    document.getElementById('referencia2').style.display= 'none';
+
+})
